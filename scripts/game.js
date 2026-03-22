40c6d6cc701607bf6b1bf370b4bf71da05adbba3
Stefan Heller <S_Heller@gmx.de>
2026-03-22 15:56:16 +0100

    feat: introduction of classes

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let world = new World();
 
 function init() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext("2d");
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
-}
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
-ctx.drawImage(character, 20, 20, 50, 150);
+  console.log("My character is", world.character);
+  console.log("Enemies", world.enemies);
+}

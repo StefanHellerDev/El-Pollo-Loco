8546c11702a95931d77c0441775857534ac05ee6
Stefan Heller <S_Heller@gmx.de>
2026-03-23 21:39:02 +0100

    show character

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,8 @@
 let canvas;
-let ctx;
-let world = new World();
 
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+  world = new World(canvas);  
 
   console.log("My character is", world.character);
   console.log("Enemies", world.enemies);

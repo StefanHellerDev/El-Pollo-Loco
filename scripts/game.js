b19799093ce615c7a11315a4781f9bc042ec99ff
Stefan Heller <S_Heller@gmx.de>
2026-03-28 20:24:52 +0100

    feat: requestAnimationFrame

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@ let canvas;
 
 function init() {
   canvas = document.getElementById("canvas");
-  world = new World(canvas);  
+  world = new World(canvas);
 
   console.log("My character is", world.character);
   console.log("Enemies", world.enemies);

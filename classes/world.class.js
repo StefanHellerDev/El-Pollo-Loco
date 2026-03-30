fd117bdb410067c9a769a2d251f3317732fa3af5
Stefan Heller <S_Heller@gmx.de>
2026-03-30 21:08:36 +0200

    feat: Hintergrundobjekte und Architektur

--- classes/world.class.js
+++ classes/world.class.js
@@ -2,6 +2,9 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject("../img/5_background/layers/3_third_layer/1.png", 0, 80)
+  ];
   canvas;
   ctx;
 
@@ -14,17 +17,25 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-    this.enemies.forEach((enemy) => {
-      this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-    });
-    this.clouds.forEach((cloud) => {
-      this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-    });
-    
+    this.addToMap(this.character);
+    this.addMultipleObjectsToMap(this.clouds);
+    this.addMultipleObjectsToMap(this.enemies);
+    this.addMultipleObjectsToMap(this.backgroundObjects);
+
+    // draw() wird immer wieder aufgerufen
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
+  }
+
+  addMultipleObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }

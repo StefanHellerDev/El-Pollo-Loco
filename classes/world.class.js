a2654b87c2522b2e2af2df187f70805b1745713f
Stefan Heller <S_Heller@gmx.de>
2026-04-07 21:15:03 +0200

    feat: enhancing the background

--- classes/world.class.js
+++ classes/world.class.js
@@ -7,10 +7,23 @@ class World {
     new BackgroundObject("../img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("../img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("../img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObject("../img/5_background/layers/air.png", 720),
+    new BackgroundObject("../img/5_background/layers/3_third_layer/2.png", 720),
+    new BackgroundObject("../img/5_background/layers/2_second_layer/2.png", 720),
+    new BackgroundObject("../img/5_background/layers/1_first_layer/2.png", 720),
+    new BackgroundObject("../img/5_background/layers/air.png", 1440),
+    new BackgroundObject("../img/5_background/layers/3_third_layer/1.png", 1440),
+    new BackgroundObject("../img/5_background/layers/2_second_layer/1.png", 1440),
+    new BackgroundObject("../img/5_background/layers/1_first_layer/1.png", 1440),
+    new BackgroundObject("../img/5_background/layers/air.png", 2160),
+    new BackgroundObject("../img/5_background/layers/3_third_layer/2.png", 2160),
+    new BackgroundObject("../img/5_background/layers/2_second_layer/2.png", 2160),
+    new BackgroundObject("../img/5_background/layers/1_first_layer/2.png", 2160),
   ];
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -27,10 +40,14 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
+
     this.addMultipleObjectsToMap(this.backgroundObjects);
     this.addMultipleObjectsToMap(this.clouds);
     this.addMultipleObjectsToMap(this.enemies);
     this.addToMap(this.character);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     // draw() wird immer wieder aufgerufen
     let self = this;

f84ce2e69199a24d5d134197ff17738bcce5e25f
Stefan Heller <S_Heller@gmx.de>
2026-05-31 20:35:07 +0200

    refactor: variables in extended classes

--- classes/bottle.class.js
+++ classes/bottle.class.js
@@ -3,13 +3,7 @@ class Bottle extends DrawableObject {
   height = 100;
   width = (this.height / 400) * 400;
   y = 480 - this.height - 50;
-  x;
-  offset = {
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  };
+  // x;
 
   constructor() {
     super().loadImage('img/6_salsa_bottle/2_salsa_bottle_on_ground.png');
--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -6,6 +6,12 @@ class DrawableObject {
   y = 280;
   height = 150;
   width = 100;
+  offset = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
 
   loadImage(path) {
     this.img = new Image();
